6912b01e7e0beff0310f7f5f192a1bbea721919c
Gandolfreddy <freddy.wei0810@gmail.com>
2022-12-25 21:06:59 +0800

    Add files via upload

--- modules/install_requires.py
+++ modules/install_requires.py
@@ -3,7 +3,7 @@ import os
 
 def install_requires():
     print("============ 安裝 requirements.txt 內容中 ============")
-    os.system("pip install -r requirements/requirements.txt")
+    os.system("pip install -r requirement/requirements.txt")
 
     print("============ 安裝 nltk 所需工具中 ============")
     import nltk
